e0b1aed86adf8012a2bb80297d6fdb52c41149b6
Darii <yakymenkodarii@gmail.com>
2025-11-10 03:33:14 +0200

    Fix: Add Issuer and Audience to TokenService

--- RealTimeChatApp.Server/Services/TokenService.cs
+++ RealTimeChatApp.Server/Services/TokenService.cs
@@ -8,28 +8,34 @@ namespace RealTimeChatApp.Server.Services
     public class TokenService : ITokenService
     {
         private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _key;
 
         public TokenService(IConfiguration configuration)
         {
             _configuration = configuration;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:IssuerSigningKey"]!));
         }
 
         public string GenerateToken(string userId, string userName)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:IssuerSigningKey"]);
 
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.NameIdentifier, userId),
-                new Claim(ClaimTypes.Name, userName),
+                new Claim(JwtRegisteredClaimNames.NameId, userId),
+                new Claim(JwtRegisteredClaimNames.UniqueName, userName)
             };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.UtcNow.AddDays(5),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                SigningCredentials = creds,
+
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
